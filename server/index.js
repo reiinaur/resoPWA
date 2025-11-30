53612d7d839054b1a142d2c23297803b7a6fbf67
arisha <113974664+reiinaur@users.noreply.github.com>
2025-11-30 02:33:36 +0000

    test 7

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ import authRouter from './auth.js';
 dotenv.config();
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -28,6 +28,6 @@ app.get('*', (req, res) => {
   res.sendFile(path.resolve(__dirname, 'dist/index.html'));
 });
 
-app.listen(PORT, '0.0.0.0', () =>
-  console.log(`Server running at http://0.0.0.0:${PORT}`)
-);
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
