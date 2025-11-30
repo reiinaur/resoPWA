9991ec92d06f8560d0b29fac65b5534c94300043
arisha <113974664+reiinaur@users.noreply.github.com>
2025-11-30 02:57:51 +0000

    test 10

--- server/index.js
+++ server/index.js
@@ -1,8 +1,5 @@
-
 import express from 'express';
 const app = express();
 const PORT = process.env.PORT || 3000;
-
 app.get('/', (req, res) => res.send('Hello Railway!'));
-
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
