69a60a81f56d51b39a43b12ce9b9a24fa23dd9c8
arisha <113974664+reiinaur@users.noreply.github.com>
2025-12-03 10:11:25 +0000

    fixed server crash git add .

--- backend/server/index.js
+++ backend/server/index.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import dotenv from 'dotenv';
-import cors from 'cors'; // Add this
+import cors from 'cors'; 
 import authRoutes from './auth.js';
 
 dotenv.config();
